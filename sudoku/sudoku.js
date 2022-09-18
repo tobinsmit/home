54ef0b494219ad67adf6a32090515621ad0e07d4
Tobin <tobin@tobinsmit.com>
2022-09-18 11:30:16 +1000

    Updated landing page and fixed sudoku

--- sudoku/sudoku.js
+++ sudoku/sudoku.js
@@ -1,4 +1,4 @@
-console.log("yep")
+console.log("Initialising sudoku")
 var newChanges = true;
 var tensor = [];
 var matrix = [];
@@ -67,6 +67,7 @@ $('#clear').click(function() {
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0]])
+  $('#allSolutions').html('')
 });
 
 function solve() {
@@ -77,9 +78,9 @@ function solve() {
 
   solveSimple();
 
-  if (confirm("Do you want to try every possibility")) {
+  // if (confirm("Do you want to try every possibility")) {
     backtrack();
-  }
+  // }
 
 }
 
@@ -129,7 +130,7 @@ function processNumber(row, col, val) {
     tensor[row][col][i] = false;
 
   }
-  fillOps();
+  // displayTensor();
 }
 
 function addNumber(row, col, val) {
@@ -291,21 +292,26 @@ $('#backtrack').click(backtrack)
 function backtrack() {
   console.log("Backtracking")
   solutions = []
+  userStoppedAtFirstSolution = false
   resetOrigMatrix();
   resetWorkMatrix();
 
   // Read input numbers
   $('.in').each(function() {
     if (1 <= $(this).val() && $(this).val() <= 9) {
-      origMatrix[$(this).parent().parent().attr('data-row')][$(this).parent().data('col')] = eval($(this).val())
-      workMatrix[$(this).parent().parent().attr('data-row')][$(this).parent().data('col')] = eval($(this).val())
+      r = $(this).parent().parent().attr('data-row')
+      c = $(this).parent().data('col')
+      origMatrix[r][c] = eval($(this).val())
+      workMatrix[r][c] = eval($(this).val())
     }
   })
 
-  console.log(checkWorkMatrixComplete())
   if (checkWorkMatrixValid(origMatrix) == false) {
+    console.log("Starting matrix invalid")
     alert("Sudoku is invalid")
     return
+  } else {
+    console.log("Starting matrix valid")
   }
 
   var spot = {r: 0, c: 0, v:1};
@@ -316,7 +322,7 @@ function backtrack() {
 
     // Keep moving if on orig number
     while (origMatrix[spot.r][spot.c] >= 1) {
-      console.log("Kept moving")
+      // console.log("Kept moving")
       moveSpot(spot);
     }
 
@@ -335,13 +341,35 @@ function backtrack() {
     }
 
     if (checkWorkMatrixComplete() == true) {
-      console.log("Found soln:")
-      console.log(workMatrix)
-      console.log(checkWorkMatrixValid())
-      console.log(checkWorkMatrixFull())
-      console.log(checkWorkMatrixComplete())
-      solutions.push(workMatrix)
+      soln = JSON.parse(JSON.stringify(workMatrix));
+      console.log("Found soln:" + soln)
+      console.log(soln)
+      console.log(matrixToString(soln))
+
+      if (checkWorkMatrixValid() == true) {
+        console.log("Marix is valid")
+      } else {
+        console.log("Marix is NOT valid")
+      }
+      if (checkWorkMatrixFull() == true) {
+        console.log("Marix is full")
+      } else {
+        console.log("Marix is NOT full")
+      }
+      if (checkWorkMatrixComplete() == true) {
+        console.log("Matrix is complete")
+      } else {
+        console.log("Matrix is NOT complete")
+      }
+      solutions.push(soln)
       spot.v = 0
+
+      showMatrix(soln)
+      
+      if (!confirm("Found a solution, do you want to continue?")) {
+        userStoppedAtFirstSolution = true
+        break
+      }
     }
 
     // Update spot
@@ -356,12 +384,31 @@ function backtrack() {
 
   console.log(`Took ${(endTime - startTime) / 1000} seconds`)
 
-  showMatrix(solutions[0])
+  showMatrix(solutions[solutions.length-1])
 
-  alert(`${solutions.length} solutions found`)
-
+  console.log("Solutions:")
+  var allSolutionsString = ""
+  for (i=0; i < solutions.length; i++) {
+    var mStr = matrixToString(solutions[i])
+    allSolutionsString = allSolutionsString + mStr
+    console.log(mStr)
+  }
   console.log(solutions)
-  console.log("Sons similarity")
+
+  if (solutions.length == 1) {
+    if (userStoppedAtFirstSolution == false) {
+      alert("1 solution found")
+    }
+
+    $('#allSolutions').html('')
+  } else if (solutions.length > 1) {
+    alert(`${solutions.length} solutions found. All will be shown at bottom of page`)
+
+    $('#allSolutions').html(allSolutionsString)
+  }
+  
+
+  if (solutions.length > 1) console.log("Solutions similarity:")
   for (i =0; i < solutions.length - 1; i++) {
     a = JSON.stringify(solutions[i])
     b = JSON.stringify(solutions[i+1])
@@ -458,7 +505,24 @@ function showMatrix(matrix) {
   ╚═╝     ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝
 */
 
-function stringValLayer(val) {
+function matrixToString(matrix) {
+  return `
+  +-------+-------+-------+
+  | ${matrix[0][0]} ${matrix[0][1]} ${matrix[0][2]} | ${matrix[0][3]} ${matrix[0][4]} ${matrix[0][5]} | ${matrix[0][6]} ${matrix[0][7]} ${matrix[0][8]} |
+  | ${matrix[1][0]} ${matrix[1][1]} ${matrix[1][2]} | ${matrix[1][3]} ${matrix[1][4]} ${matrix[1][5]} | ${matrix[1][6]} ${matrix[1][7]} ${matrix[1][8]} |
+  | ${matrix[2][0]} ${matrix[2][1]} ${matrix[2][2]} | ${matrix[2][3]} ${matrix[2][4]} ${matrix[2][5]} | ${matrix[2][6]} ${matrix[2][7]} ${matrix[2][8]} |
+  +-------+-------+-------+
+  | ${matrix[3][0]} ${matrix[3][1]} ${matrix[3][2]} | ${matrix[3][3]} ${matrix[3][4]} ${matrix[3][5]} | ${matrix[3][6]} ${matrix[3][7]} ${matrix[3][8]} |
+  | ${matrix[4][0]} ${matrix[4][1]} ${matrix[4][2]} | ${matrix[4][3]} ${matrix[4][4]} ${matrix[4][5]} | ${matrix[4][6]} ${matrix[4][7]} ${matrix[4][8]} |
+  | ${matrix[5][0]} ${matrix[5][1]} ${matrix[5][2]} | ${matrix[5][3]} ${matrix[5][4]} ${matrix[5][5]} | ${matrix[5][6]} ${matrix[5][7]} ${matrix[5][8]} |
+  +-------+-------+-------+
+  | ${matrix[6][0]} ${matrix[6][1]} ${matrix[6][2]} | ${matrix[6][3]} ${matrix[6][4]} ${matrix[6][5]} | ${matrix[6][6]} ${matrix[6][7]} ${matrix[6][8]} |
+  | ${matrix[7][0]} ${matrix[7][1]} ${matrix[7][2]} | ${matrix[7][3]} ${matrix[7][4]} ${matrix[7][5]} | ${matrix[7][6]} ${matrix[7][7]} ${matrix[7][8]} |
+  | ${matrix[8][0]} ${matrix[8][1]} ${matrix[8][2]} | ${matrix[8][3]} ${matrix[8][4]} ${matrix[8][5]} | ${matrix[8][6]} ${matrix[8][7]} ${matrix[8][8]} |
+  +-------+-------+-------+`
+}
+
+function displayTensorLayer(val) {
   return `
   +-------+-------+-------+
   | ${tensor[0][0][val-1]?val:' '} ${tensor[0][1][val-1]?val:' '} ${tensor[0][2][val-1]?val:' '} | ${tensor[0][3][val-1]?val:' '} ${tensor[0][4][val-1]?val:' '} ${tensor[0][5][val-1]?val:' '} | ${tensor[0][6][val-1]?val:' '} ${tensor[0][7][val-1]?val:' '} ${tensor[0][8][val-1]?val:' '} |
@@ -475,30 +539,29 @@ function stringValLayer(val) {
   +-------+-------+-------+`
 }
 
-function fillOps() {
-  $('#ops').html(`
+function displayTensor() {
+  $('#tensor').html(`
     <div class="col-4">
       <pre>
-        ${stringValLayer(1)}
-        ${stringValLayer(4)}
-        ${stringValLayer(7)}
+        ${displayTensorLayer(1)}
+        ${displayTensorLayer(4)}
+        ${displayTensorLayer(7)}
       </pre>
     </div>
     <div class="col-4">
       <pre>
-        ${stringValLayer(2)}
-        ${stringValLayer(5)}
-        ${stringValLayer(8)}
+        ${displayTensorLayer(2)}
+        ${displayTensorLayer(5)}
+        ${displayTensorLayer(8)}
       </pre>
     </div>
     <div class="col-4">
       <pre>
-        ${stringValLayer(3)}
-        ${stringValLayer(6)}
-        ${stringValLayer(9)}
+        ${displayTensorLayer(3)}
+        ${displayTensorLayer(6)}
+        ${displayTensorLayer(9)}
       </pre>
     </div>
-
   `)
 }
 
